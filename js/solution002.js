d92bd6caa729f1cd5ee266089cb157210e363c88
jazzypants1989 <102560332+jazzypants1989@users.noreply.github.com>
2022-05-10 14:35:39 -0700

    big old bongos! solution 002 complete

---
+++ js/solution002.js
@@ -0,0 +1,6 @@
+/*
+Write a function called repeatStr which repeats the given string string exactly n times.
+*/
+let repeatStr = (n, s) => `${s.repeat(n)}`;
+
+console.log(repeatStr(9, 'big old bongos! '))
