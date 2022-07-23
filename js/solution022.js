02c8415530fda5a73d9cbceb6d4fa4c75cf98627
jazzypants1989 <jessepence@gmail.com>
2022-07-22 23:48:57 -0700

    22

---
+++ js/solution022.js
@@ -0,0 +1,38 @@
+/*
+Knight vs King
+
+If you are not familiar with chess game you can learn more Here .
+
+Here is the chess board (rows, denoted by numbers, are called ranks, columns, denoted by a letter, are called files):
+
+alt text
+
+You put a Knight and a King in the board.
+
+Complete the method that tell us which one can capture the other one.
+
+You are provided with two object array; each contains an integer (the rank, first item) and a string/char (the file, second item) to show the position of the pieces in the chess board.
+
+Return:
+
+    "Knight" if the knight is putting the king in check,
+    "King" if the king is attacking the knight
+    "None" if none of the above occur
+
+Example:
+
+knight = [7, "B"], king = [6, "C"]  ---> "King"
+
+Check the test cases and Happy coding :)
+
+*/
+function knightVsKing(R, K) {
+    let dx = R[0] - K[0]
+    let dy = R[1].charCodeAt() - K[1].charCodeAt()
+    let d = dx * dx + dy * dy
+    if (d === 5)
+        return 'Knight'
+    if (d < 3)
+        return 'King'
+    return 'None'
+}
