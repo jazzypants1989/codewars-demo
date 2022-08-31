060752c04e32f58a32f2686e157599a01bce7990
jazzypants1989 <jessepence@gmail.com>
2022-08-31 11:05:38 -0700

    that was not annoying at all

---
+++ js/solution053 - Ascend, Descend, Repeat.js
@@ -0,0 +1,36 @@
+/* Description:
+
+You are given three integer inputs: length, minimum, and maximum.
+
+Return a string that:
+
+    Starts at minimum
+    Ascends one at a time until reaching the maximum, then
+    Decends one at a time until reaching the minimum
+    repeat until the string is the appropriate length
+
+Examples:
+
+ length: 5, minimum: 1, maximum: 3   ==>  "12321"
+ length: 14, minimum: 0, maximum: 2  ==>  "01210121012101"
+ length: 11, minimum: 5, maximum: 9  ==>  "56789876567"
+
+Notes:
+
+    length will always be non-negative
+    negative numbers can appear for minimum and maximum values
+    hyphens/dashes ("-") for negative numbers do count towards the length
+    the resulting string must be truncated to the exact length provided
+    return an empty string if maximum < minimum or length == 0
+    minimum and maximum can equal one another and result in a single number repeated for the length of the string
+
+
+*/
+
+function ascendDescend(len, min, max) {
+    var res = "";
+    for (var i = min; i < max; i++) res += i;
+    for (var j = max; j > min; j--) res += j;
+    if (min === max) res = min.toString();
+    return res.repeat(len).slice(0, len);
+}
