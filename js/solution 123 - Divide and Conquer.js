46efc7e58fa42bd5930f216d4cf9ebc4bca55125
jazzypants1989 <jessepence@gmail.com>
2022-09-14 05:21:11 -0700

    whoops, wrong description

--- js/solution 123 - Divide and Conquer.js
+++ js/solution 123 - Divide and Conquer.js
@@ -1,7 +1,9 @@
 /*
-Modify the spacify function so that it returns the given string with spaces inserted between each character.
 
-spacify("hello world") // returns "h e l l o   w o r l d"
+Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+
+Return as a number.
+
 
 */
 
