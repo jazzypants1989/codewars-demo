6442e1488f5c2ee15f61e0519670b99151cbdff2
Jesse Pence <jessepence@gmail.com>
2023-07-30 07:00:00 +0000

    wowee

---
+++ js/solution005 - Name Shuffler.js
@@ -0,0 +1,3 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/javascript
+const nameShuffler = (s) => s.split("").reverse().join("")
--- js/solution005.js
+++
@@ -1,2 +0,0 @@
-// Write a function that returns a string in which firstname is swapped with last name.
-    return s.split("").reverse().join("");
