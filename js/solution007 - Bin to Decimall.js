3ad0119b0eae8f3b2455485956566ccba5c94b23
Jesse Pence <jessepence@gmail.com>
2023-08-01 07:00:00 +0000

    007

---
+++ js/solution007 - Bin to Decimall.js
@@ -0,0 +1,6 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
+function binToDec(bin) {
+  let cheese = parseInt(bin, 2)
+  return cheese
+}
--- js/solution007.js
+++
@@ -1,5 +0,0 @@
-//Complete the function which converts a binary number (given as a string) to a decimal number.
-function binToDec(bin){
-    let cheese = parseInt(bin, 2)
-    return cheese
-  }
