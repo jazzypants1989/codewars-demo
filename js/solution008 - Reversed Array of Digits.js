8269ba4b4a56d3670934b85a2db6d9c23d6d4460
Jesse Pence <jessepence@gmail.com>
2023-08-02 07:00:00 +0000

    008

--- js/solution008 - Reversed Array of Digits.js
+++ js/solution008 - Reversed Array of Digits.js
@@ -1,8 +1,10 @@
 /*Convert number to reversed array of digits
 
 Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+https://www.codewars.com/kata/5583090cbe83f4fd8c000051
 */
 
 function digitize(n) {
-    return String(n).split('').map(Number).reverse()
-  }
+  return String(n).split("").map(Number).reverse()
+}
