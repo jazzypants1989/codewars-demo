39a58614fbc1922288b6e1028e6138147b95abc6
Jesse Pence <jessepence@gmail.com>
2023-08-11 07:00:00 +0000

    017

--- js/solution017 - Even Numbers in an Array.js
+++ js/solution017 - Even Numbers in an Array.js
@@ -1,7 +1,9 @@
 /*
 Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c
 */
 
 function evenNumbers(array, number) {
-    return array.filter(num => num % 2 == 0).slice(-number)
-  }
+  return array.filter((num) => num % 2 == 0).slice(-number)
+}
