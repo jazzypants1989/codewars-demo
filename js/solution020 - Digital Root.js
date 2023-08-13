21865d13b1646c4458a166e821b28fd51db5543a
Jesse Pence <jessepence@gmail.com>
2023-08-13 07:00:00 +0000

    019

--- js/solution020 - Digital Root.js
+++ js/solution020 - Digital Root.js
@@ -6,20 +6,22 @@ Examples
     16  -->  1 + 6 = 7
    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+
+https://www.codewars.com/kata/541c8630095125aba6000c00
 */
 
 function digital_root(n) {
-    if (n < 10) {
-      return n
-    }
-    
-    let newStr = String(n).split('')
-    const newNum = newStr => {
-      let number = 0
-      for (let i=0; i<newStr.length; i++){
-        number += Number(newStr[i])
-      }
-      return number
-    }
-    return digital_root(newNum(newStr))
+  if (n < 10) {
+    return n
   }
+
+  let newStr = String(n).split("")
+  const newNum = (newStr) => {
+    let number = 0
+    for (let i = 0; i < newStr.length; i++) {
+      number += Number(newStr[i])
+    }
+    return number
+  }
+  return digital_root(newNum(newStr))
+}
