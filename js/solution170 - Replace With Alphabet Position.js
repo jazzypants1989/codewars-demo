02498a762e60f125aa2ac5b88c0e84563634d17f
Jesse Pence <jessepence@gmail.com>
2024-05-06 07:00:00 +0000

    CODEWARS!!!!

---
+++ js/solution170 - Replace With Alphabet Position.js
@@ -0,0 +1,53 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// Input = "The sunset sets at twelve o' clock."
+// Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+// https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
+function alphabetPosition(text) {
+  let numbers = ""
+  for (const letter of text) {
+    let lowercase = letter.toLowerCase()
+    if (alphabet.includes(lowercase)) {
+      let number = alphabet.indexOf(lowercase) + 1
+      numbers = numbers.concat(" ", number.toString())
+    }
+  }
+  return numbers.trim()
+}
