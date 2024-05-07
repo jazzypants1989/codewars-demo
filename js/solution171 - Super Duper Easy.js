a3a3e79bb19e3054c5e20c465258f41f84675adb
Jesse Pence <jessepence@gmail.com>
2024-05-07 07:00:00 +0000

    CODEWARS!!!!

---
+++ js/solution171 - Super Duper Easy.js
@@ -0,0 +1,5 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+// https://www.codewars.com/kata/55a5bfaa756cfede78000026
+
+const problem = (x) => (typeof x === "string" ? "Error" : x * 50 + 6)
