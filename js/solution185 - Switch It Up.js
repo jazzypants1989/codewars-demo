8d800859404054e1aae1b09259597585de790986
Jesse Pence <jessepence@gmail.com>
2024-05-19 07:00:00 +0000

    CODEWARS!!!!

---
+++ js/solution185 - Switch It Up.js
@@ -0,0 +1,25 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number) {
+  const dictionary = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+  }
+  const string = number.toString()
+
+  return dictionary[string]
+}
