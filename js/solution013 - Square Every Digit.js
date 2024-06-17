dac0082b44c3c3f489b3831d3f0136f913285be0
Jesse Pence <jessepence@gmail.com>
2024-06-17 02:23:58 -0700

    013

--- js/solution013 - Square Every Digit.js
+++ js/solution013 - Square Every Digit.js
@@ -3,14 +3,15 @@
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
 Note: The function accepts an integer and returns an integer
+https://www.codewars.com/kata/546e2562b03326a88e000020
 */
 
-function squareDigits(num){
-    const numStr = String(num);
-    let res = '';
-    for(let i = 0; i < numStr.length; i++){
-       const square = Math.pow(+numStr[i], 2);
-       res += square;
-    };
-    return Number(res);
- };
+function squareDigits(num) {
+  const numStr = String(num)
+  let res = ""
+  for (let i = 0; i < numStr.length; i++) {
+    const square = Math.pow(+numStr[i], 2)
+    res += square
+  }
+  return Number(res)
+}
